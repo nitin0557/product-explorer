4a0ecdd318f47631901cbda7a7419338b4400e21
nitin0778 <singh.ntn009@gmail.com>
2026-01-14 18:09:02 +0530

    config

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  reactCompiler: true,
   images: {
-    domains: ['fakestoreapi.com'], // replace with your API image domain
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "fakestoreapi.com",
+        pathname: "/**",
+      },
+    ],
   },
 };
 
